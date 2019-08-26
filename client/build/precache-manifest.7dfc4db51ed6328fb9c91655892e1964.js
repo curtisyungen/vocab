976a8cb840e745ff7cdf8694ba2640ecae8175f8
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-25 18:22:23 -0700

    Add files via upload

---
+++ client/build/precache-manifest.7dfc4db51ed6328fb9c91655892e1964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ea0765af6dc372dc4409",
+    "url": "/static/js/main.4c9b01e5.chunk.js"
+  },
+  {
+    "revision": "0ad90b1e5587d8b85af5",
+    "url": "/static/js/2.be3e6675.chunk.js"
+  },
+  {
+    "revision": "ea0765af6dc372dc4409",
+    "url": "/static/css/main.e96addff.chunk.css"
+  },
+  {
+    "revision": "23e27b4a9e8180d0fb96350286b09f29",
+    "url": "/index.html"
+  }
+];
